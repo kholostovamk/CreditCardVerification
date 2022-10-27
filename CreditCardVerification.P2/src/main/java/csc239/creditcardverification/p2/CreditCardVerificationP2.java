2043cb90da2c5d594cf2a1dce56445e6e65c3f6c
Margo <kholostova@gmail.com>
2022-10-27 19:40:42 -0400

    Intro for the first run of the program (possibly change it into separate method later)

--- CreditCardVerification.P2/src/main/java/csc239/creditcardverification/p2/CreditCardVerificationP2.java
+++ CreditCardVerification.P2/src/main/java/csc239/creditcardverification/p2/CreditCardVerificationP2.java
@@ -12,9 +12,22 @@ package csc239.creditcardverification.p2;
 public class CreditCardVerificationP2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
-    }
-    checking git
-            
+        //intro for 1st session (maybe put in separate function later)
+        System.out.println("This program accepts the following inputs and performs the corresponding actions:\n");
+        System.out.println("USAGE:   command parameters(s)");
+        System.out.printf("   %-10s %-10s\n", "COMMAND", "PARAMETER(S)");
+        System.out.printf("   %-10s %-10s %-10s\n", "help", "\t\t", "Output help text.");
+        System.out.printf("   %-10s %-10s %-10s\n", "create", "issuerSymbol", "\t CREATE new account.");
+        System.out.printf("   %-10s %-10s %-10s\n", "verify", "accountNum amount", " VERIFY a purchase or credit.");
+        System.out.printf("   %-10s %-10s %-10s\n", "q", "\t\t", "exit the program.\n");
+        System.out.println("The command and issuerSymbol values are NOT case sensitive.");
+        System.out.println("The following credit cards are supported:");
+        System.out.printf("   %-20s %-10s\n", "CARD", "SYMBOL");
+        System.out.printf("   %-20s %-10s\n", "American Express", "AE");
+        System.out.printf("   %-20s %-10s\n", "Visa", "V");
+        System.out.printf("   %-20s %-10s\n", "MasterCard", "MC");
+        System.out.printf("   %-20s %-10s\n", "Discover", "DIS");
+        System.out.printf("   %-20s %-10s\n", "Diners Club", "DINE");
+    }      
 }
